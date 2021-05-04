7297f6269ebdb78b03b85ac71036d648ee3755b0
gary <gary0928@gmail.com>
2021-05-04 15:32:47 -0700

    Revert to functional component

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,34 +1,17 @@
 import React from 'react'
-import clsx from 'clsx'
 
-import {withStyles} from '@material-ui/core/styles'
 import {makeStyles} from '@material-ui/core/styles'
 import '@fontsource/roboto'
 import {AppBar} from '@material-ui/core'
 import {Toolbar} from '@material-ui/core'
 import {IconButton} from '@material-ui/core'
 import {Typography} from '@material-ui/core'
-import {Drawer} from '@material-ui/core'
 import {Button} from '@material-ui/core'
 import {Grid} from '@material-ui/core'
-import {List} from '@material-ui/core'
-import {ListItem} from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
 import MeetingRoom from '@material-ui/icons/MeetingRoom'
 
-// const useStyles = makeStyles((theme) => ({
-//     root: {
-//         flexGrow: 1,
-//     },
-//     menuButton: {
-//         marginRight: theme.spacing(2),
-//     },
-//     title: {
-//         marginRight: theme.spacing(2)
-//     },
-//   }));
-
-const styles = theme => ({
+const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1
     },
@@ -38,48 +21,11 @@ const styles = theme => ({
     title: {
         marginRight: theme.spacing(2)
     },
-});
+}));
 
-// function Sidebar_drawer(){
-//     // const classes = useStyles();
-//     const [state, setState] = React.useState({
-//         top: false,
-//         left: false,
-//         bottom: false,
-//         right: false,
-//     });
-
-//     const toggleDrawer = (anchor, open) => (event) => {
-//         if(event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')){
-//             return;
-//         }
-//         setState({ ...state, [anchor]:open});
-//     }
-
-//     const list = (anchor) => (
-//         <div
-//             className={clsx(classes.list, {
-//                 [classes.fullList]: anchor === 'top' || anchor === 'bottom'
-//             })}
-//             role="presentation"
-//             onClick={toggleDrawer(anchor, false)}
-//             onKeyDown={toggleDrawer(anchor, false)}
-//         >
-//             <List>
-//                 {['A', 'B', 'C', 'D'].map((text, index) => (
-//                     <ListItem button key={text}>
-//                     </ListItem>
-//                 ))}
-//             </List>
-//         </div>
-//     )
-// }
-
-class Sidebar extends React.Component{
-    // const classes = useStyles();
-    render(){
-        const {classes} = this.props;
-        return(
+function Sidebar(){
+    const classes = useStyles();
+    return(
         <div className={classes.root}>
         <AppBar position="static" color="primary">
             <Toolbar>
@@ -118,8 +64,7 @@ class Sidebar extends React.Component{
              </Toolbar>
         </AppBar>
         </div>
-        );
-    }
+    );
 }
 
-export default withStyles(styles)(Sidebar);
+export default Sidebar;

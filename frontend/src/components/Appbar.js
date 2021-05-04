25dd5eb6437be88c7196e37216b740e578d1156e
gary <gary0928@gmail.com>
2021-05-04 16:06:11 -0700

    Create TempDrawer -- rudimentary appbar complete

--- frontend/src/components/Appbar.js
+++ frontend/src/components/Appbar.js
@@ -1,22 +1,17 @@
 import React from 'react'
-
+import TempDrawer from './TempDrawer.js'
 import {makeStyles} from '@material-ui/core/styles'
 import '@fontsource/roboto'
 import {AppBar} from '@material-ui/core'
 import {Toolbar} from '@material-ui/core'
-import {IconButton} from '@material-ui/core'
 import {Typography} from '@material-ui/core'
 import {Button} from '@material-ui/core'
 import {Grid} from '@material-ui/core'
-import MenuIcon from '@material-ui/icons/Menu'
 import MeetingRoom from '@material-ui/icons/MeetingRoom'
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
     },
     title: {
         marginRight: theme.spacing(2)
@@ -30,9 +25,7 @@ function Appbar(){
         <AppBar position="static" color="primary">
             <Toolbar>
                 {/*"Hamburger" menu icon*/}
-                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                    <MenuIcon />
-                </IconButton>
+                <TempDrawer/>
 
                 {/*Title*/}
                 <Grid

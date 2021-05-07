c43cf2440cf4f300f71b073bd6faf63d02f260d8
gary <gary0928@gmail.com>
2021-05-06 17:51:18 -0700

    Update header, fixed width, appearance

--- frontend/src/components/TransactionTable.js
+++ frontend/src/components/TransactionTable.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -8,9 +8,29 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+    align: "right",
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
 const useStyles = makeStyles({
   table: {
-    minWidth: 650,
+    minWidth: 700,
+    maxWidth: 700,
   },
 });
 
@@ -33,24 +53,24 @@ function TransactionTable() {
       <Table className={classes.table} aria-label="a simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell>Calories</TableCell>
-            <TableCell>Fat&nbsp;(g)</TableCell>
-            <TableCell>Carbs&nbsp;(g)</TableCell>
-            <TableCell>Protein&nbsp;(g)</TableCell>
+            <StyledTableCell>Date</StyledTableCell>
+            <StyledTableCell>Description</StyledTableCell>
+            <StyledTableCell>Category</StyledTableCell>
+            <StyledTableCell align="right">Amount</StyledTableCell>
+            <StyledTableCell align="right">Remaining Balance</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <TableRow key={row.date}>
+            <StyledTableRow key={row.date}>
               <TableCell component="th" scope="row">
                 {row.date}
               </TableCell>
               <TableCell>{row.desc}</TableCell>
               <TableCell>{row.category}</TableCell>
-              <TableCell>{row.amount}</TableCell>
-              <TableCell>{row.balance}</TableCell>
-            </TableRow>
+              <TableCell align="right">{row.amount}</TableCell>
+              <TableCell align="right">{row.balance}</TableCell>
+            </StyledTableRow>
           ))}
         </TableBody>
       </Table>

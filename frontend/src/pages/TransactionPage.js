390ae8652f89c5a0d764e09ce70743ebde7a734d
gary <gary0928@gmail.com>
2021-05-30 16:44:03 -0700

    Update remaining balance upon transaction POST

--- frontend/src/pages/TransactionPage.js
+++ frontend/src/pages/TransactionPage.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import Appbar from '../components/Appbar.js';
-import TransactionForm from '../components/TransactionForm.js';
-import TransactionTable from '../components/TransactionTable.js';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import Appbar from "../components/Appbar.js";
+import TransactionForm from "../components/TransactionForm.js";
+import TransactionTable from "../components/TransactionTable.js";
+import Grid from "@material-ui/core/Grid";
 
-function TransactionPage(){
+function TransactionPage() {
     const [transactions, setTransactions] = React.useState([]);
-    const axios = require('axios');
-    
-    async function removeOneTransaction (index) {
-        const _id = transactions[index]['_id']
-        try{
-            const response = await axios.delete('http://localhost:5000/users/60a483f4cc4a814ce0cb4139/transactions/'.concat(_id));
-        }
-        catch(error){
+    const axios = require("axios");
+
+    async function removeOneTransaction(index) {
+        const _id = transactions[index]["_id"];
+        try {
+            const response = await axios.delete(
+                "http://localhost:5000/users/60a483f4cc4a814ce0cb4139/transactions/".concat(
+                    _id
+                )
+            );
+        } catch (error) {
             //We're not handling errors. Just logging into the console.
             console.log(error);
         }
         const updated = transactions.filter((character, i) => {
-            return i !== index
+            return i !== index;
         });
         setTransactions(updated);
-    };
+    }
     function compareDates(a, b) {
         var resA = a.date.split("-");
         var resB = b.date.split("-");
@@ -45,46 +48,71 @@ function TransactionPage(){
         }
         return 0;
     }
-    
+
     function updateList(transaction) {
-        makePostCall(transaction).then(result => {
-            if(result)
-                setTransactions([...transactions, result.data].sort(compareDates));
+        makePostCall(transaction).then((result) => {
+            if (result)
+                setTransactions(
+                    [...transactions, result.data].sort(compareDates)
+                );
         });
     }
 
     async function makePostCall(transaction) {
         try {
-            const response = await axios.post('http://localhost:5000/users/60a483f4cc4a814ce0cb4139/transactions', transaction);
-            return response;
-        }
-        catch(error) {
+            let postOne =
+                "http://localhost:5000/users/60a483f4cc4a814ce0cb4139/transactions";
+            let patchTwo =
+                "http://localhost:5000/users/60a483f4cc4a814ce0cb4139/remainingBalance";
+
+            const requestPostOne = axios.post(postOne, transaction);
+            const requestPatchTwo = axios.patch(patchTwo, transaction);
+
+            axios.all([requestPostOne, requestPatchTwo]).then(
+                axios.spread((...responses) => {
+                    const responsePostOne = responses[0];
+                    const responsePatchTwo = responses[1];
+                })
+            );
+            // const response = await axios.post('http://localhost:5000/users/60a483f4cc4a814ce0cb4139/transactions', transaction);
+            // const response = await axios.patch(
+            //     "http://localhost:5000/users/60a483f4cc4a814ce0cb4139/remainingBalance",
+            //     transaction
+            // );
+            // return response;
+        } catch (error) {
             console.log(error);
             return false;
         }
     }
-    
-    async function fetchAll(){
+
+    async function fetchAll() {
         try {
-            const response = await axios.get('http://localhost:5000/users/60a483f4cc4a814ce0cb4139/transactions');
+            const response = await axios.get(
+                "http://localhost:5000/users/60a483f4cc4a814ce0cb4139/transactions"
+            );
             const updated = response.data.transaction_list.sort(compareDates);
             return updated;
-        }
-        catch (error){
+        } catch (error) {
             //We're not handling errors. Just logging into the console.
-            console.log(error); 
-            return false;         
+            console.log(error);
+            return false;
         }
-    };
-    
-    React.useEffect(() => {
-        fetchAll().then(result => {
-            if (result)
-                setTransactions(result);
-        });
-    }, [] );
+    }
 
-    return(
+    // async function getRemaining(){
+    //     try{
+    //         const response = await axios.get('')
+    //     }
+    // }
+
+    React.useEffect(() => {
+        fetchAll().then((result) => {
+            if (result) setTransactions(result);
+        });
+    }, []);
+
+    return (
         <div className="transaction-page">
             <Appbar />
             <Grid
@@ -93,18 +121,19 @@ function TransactionPage(){
                 justify="flex-end"
                 alignItems="right"
             >
-            <TransactionForm handleSubmit={updateList} />
-
-            </Grid> 
+                <TransactionForm handleSubmit={updateList} />
+            </Grid>
 
             <Grid
                 container
                 direction="row"
                 justify="flex-end"
-                alignItems="right" 
+                alignItems="right"
             >
-                <TransactionTable  transactionData={transactions} 
-                    removeTransaction={removeOneTransaction}/>
+                <TransactionTable
+                    transactionData={transactions}
+                    removeTransaction={removeOneTransaction}
+                />
             </Grid>
         </div>
     );

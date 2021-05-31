91f5a129c5fe1cc2004da68d7f8d74b69317226e
Cody Shanahan <cody.33shan@gmail.com>
2021-05-30 17:12:15 -0700

    Bar Chart working with hard-coded data

--- frontend/src/components/BarChart.js
+++ frontend/src/components/BarChart.js
@@ -14,11 +14,10 @@ function BarChart() {
                      "May", "June", "July", "August",
                      "September", "October", "November", "December"],
             datasets: [{
-                label: "Amount",
                 // data: [amts.January, amts.February, amts.March, amts.April,
                 //        amts.May, amts.June, amts.July, amts.August,
                 //        amts.September, amts.October, amts.November, amts.December],
-                data: [100, 200, 300, 200, 150, 230, 333, 777, 123, 563, 345, 500],
+                data: [100, 200, 300, 200, 150, 222, 225, 777, 123, 563, 345, 500],
                 backgroundColor: [red[500], pink[500], purple[500], deepPurple[500],
                                   blue[500], lightBlue[500], cyan[500], teal[500], 
                                   green[500], lightGreen[500], yellow[500], orange[500]],
@@ -27,13 +26,27 @@ function BarChart() {
                 blue[900], lightBlue[900], cyan[900], teal[900], 
                 green[900], lightGreen[900], yellow[900], orange[900]]
             }]
+        },
+        dataBarOptions: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
         }
+
     }
 
     return (
         <MDBContainer>
             <h3 className="mt-5">Total Spendings By Month</h3>
-            <Bar data={state.dataBar} options={{ responsive: true }} />
+            <Bar data={state.dataBar} options={state.dataBarOptions} />
         </MDBContainer>
     )
 }
+
+export default BarChart;

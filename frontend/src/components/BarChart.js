d8ac80aa9d86967ff75d91052d275bef4618ccca
Cody Shanahan <cody.33shan@gmail.com>
2021-05-31 09:32:04 -0700

    Some small changes

--- frontend/src/components/BarChart.js
+++ frontend/src/components/BarChart.js
@@ -4,12 +4,14 @@ import { MDBContainer } from "mdbreact";
 import axios from "axios";
 import {red, orange, yellow, green, blue, purple, deepPurple, 
     pink, lightBlue, cyan, teal, lightGreen} from '@material-ui/core/colors';
+import Button from '@material-ui/core/Button';
+
 
 const userID = "60a483f4cc4a814ce0cb4139";
-var currYear = 2021;
+const year = 2021;
 
 function BarChart() {
-    const [amts, setAmts] = useState([])
+    const [amts, setAmts] = useState([]);
 
     function filter_by_date(list, month, year) {
         var updated = [];
@@ -26,7 +28,7 @@ function BarChart() {
     async function getMonthSpending(month, year) {
         return (axios.get('http://localhost:5000/users/' + userID + '/transactions?spent=1')
                 .then(response => {
-                    var amt = 0;
+                    var amt = 0
                     const list = response.data.transaction_list
                     const updated = filter_by_date(list, month, year)
                     for (var i=0; i<updated.length; i++){
@@ -52,51 +54,51 @@ function BarChart() {
             November: 0,
             December: 0
         }
-        getMonthSpending(0, currYear).then(result => {
+        getMonthSpending(0, year).then(result => {
             monthArr["January"] = result
             console.log("January: " + result)
         })
-        getMonthSpending(1, currYear).then(result => {
+        getMonthSpending(1, year).then(result => {
             monthArr["February"] = result
             console.log("February: " + result)
         })
-        getMonthSpending(2, currYear).then(result => {
+        getMonthSpending(2, year).then(result => {
             monthArr["March"] = result
             console.log("March: " + result)
         })
-        getMonthSpending(3, currYear).then(result => {
+        getMonthSpending(3, year).then(result => {
             monthArr["April"] = result
             console.log("April: " + result)
         })
-        getMonthSpending(4, currYear).then(result => {
+        getMonthSpending(4, year).then(result => {
             monthArr["May"] = result
             console.log("May: " + result)
         })
-        getMonthSpending(5, currYear).then(result => {
+        getMonthSpending(5, year).then(result => {
             monthArr["June"] = result
             console.log("June: " + result)
         })
-        getMonthSpending(6, currYear).then(result => {
+        getMonthSpending(6, year).then(result => {
             monthArr["July"] = result
             console.log("July: " + result)
         })
-        getMonthSpending(7, currYear).then(result => {
+        getMonthSpending(7, year).then(result => {
             monthArr["August"] = result
             console.log("August: " + result)
         })
-        getMonthSpending(8, currYear).then(result => {
+        getMonthSpending(8, year).then(result => {
             monthArr["September"] = result
             console.log("September: " + result)
         })
-        getMonthSpending(9, currYear).then(result => {
+        getMonthSpending(9, year).then(result => {
             monthArr["October"] = result
             console.log("October: " + result)
         })
-        getMonthSpending(10, currYear).then(result => {
+        getMonthSpending(10, year).then(result => {
             monthArr["November"] = result
             console.log("November: " + result)
         })
-        getMonthSpending(11, currYear).then(result => {
+        getMonthSpending(11, year).then(result => {
             monthArr["December"] = result
             setAmts(monthArr)
             console.log("December: " + result)
@@ -138,7 +140,7 @@ function BarChart() {
 
     return (
         <MDBContainer>
-            <h3 className="mt-5">Total Spendings By Month (2021)</h3>
+            <h3 className="mt-5">Total Spendings By Month ({year})</h3>
             <Bar data={state.dataBar} options={state.dataBarOptions} />
         </MDBContainer>
     )

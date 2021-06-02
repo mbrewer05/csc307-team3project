9f9ef0ec8618f7984555950430cd3754ecb82139
gary <gary0928@gmail.com>
2021-06-01 21:00:36 -0700

    Add RemainingBalance tests

--- backend/sampleTest.py
+++ backend/sampleTest.py
@@ -1,4 +1,26 @@
 import pytest
+from flaskMongo import *
 
-def test_1():
-   assert 1 == 1
+testUserID = "60b6fa44b65b73db02a27417"
+
+""" 
+    PLEASE structure tests so 'balance' will be 0 at the end
+"""
+def test_RemBal_get_val():
+   assert RemainingBalance().get_val(testUserID)[0]['balance'] == 0
+
+
+def test_RemBal_add_to_balance():
+   preAdd = RemainingBalance().get_val(testUserID)[0]['balance']
+   RemainingBalance().add_to_balance(testUserID, 10)
+   postAdd = RemainingBalance().get_val(testUserID)[0]['balance']
+
+   assert postAdd == preAdd + 10
+
+
+def test_RemBal_sub_from_balance():
+   preAdd = RemainingBalance().get_val(testUserID)[0]['balance']
+   RemainingBalance().sub_from_balance(testUserID, 10)
+   postAdd = RemainingBalance().get_val(testUserID)[0]['balance']
+
+   assert postAdd == preAdd - 10
